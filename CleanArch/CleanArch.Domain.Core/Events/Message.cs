cc2e97ae8d4af1e7f060705fb260e7b9b45e9513
SUDARSHAN NEUPANE <sudnep@terpmail.umd.edu>
2020-07-11 15:27:03 -0400

    domain core and in memeory bus

---
+++ CleanArch/CleanArch.Domain.Core/Events/Message.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Core.Events
+{
+
+    public abstract class Message : IRequest<bool> // returns bool, ,we can make if of any type, in our case is bool
+    {
+       
+        public string MessageType { get; protected set; }
+        public Message()
+        {
+            // MessageType is a property
+            // GetType Get object
+            MessageType = GetType().Name;
+
+        }
+    }
+}
--- CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
+++ CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CleanArch.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler // sealed class  nobody can extend or add functionality  
     {
+        private readonly IMediator _mediator;
+
+        public InMemoryBus(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+       
+        public Task SendCommand<T> (T command) where T: Command
+        {
+            return _mediator.Send(command);
+        }
+        
     }
+
+    
 }
